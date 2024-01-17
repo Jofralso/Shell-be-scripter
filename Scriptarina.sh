9844da636907f5e5113eb71887b52a5453368291
Joao Francisco Almeida Soares <fsoares@estgl.ipv.pt>
2024-01-17 17:11:53 +0000

    added Scriptarina.sh

---
+++ Scriptarina.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Scriptarina.sh - The Grand Ringmaster of Shell-be Scripter Circus
+
+echo "Welcome to the Scriptarina.sh Show - Where Scripts Dance and Files Prance!"
+echo "🎭🤹‍♂️🚀"
+
+# Function to create a folder with a festive touch
+create_festive_folder() {
+  folder_name=$1
+  mkdir "$folder_name"
+  echo "Ta-da! The folder '$folder_name' has been created with flair!"
+}
+
+# Main script performance
+echo "Now, let's create some festive folders:"
+create_festive_folder "JugglingJubilee"
+create_festive_folder "AcrobaticArchive"
+create_festive_folder "LaughingLibrary"
+
+echo "Scriptarina.sh has concluded the spectacular show. Bravo! Encore! 🎉"
